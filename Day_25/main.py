d80afaf3138e99df094ddeaa6a309ff0453a81ce
Miftah Jabeen <avengerpak77@gmail.com>
2024-07-20 21:05:29 +0500

    csv file

--- Day_25/main.py
+++ Day_25/main.py
@@ -1,58 +1,35 @@
-# # with open("weather_data(in).csv") as data:
-# #     data1= data.readlines()
-# #     print(data1)
-#
-# # import csv
-# # with open("weather_data(in).csv") as data:
-# #     data1=csv.reader(data)
-# #     temperatures=[]
-# #     for row in data1:
-# #        if row[1]!="temp":
-# #            temperatures.append(row[1])
-# #            print(temperatures)
-# import pandas
-# data=pandas.read_csv("weather_data(in).csv")
-# # print(type(data["temp"]))
-# # data_dict=data.to_dict()
-# # print(data_dict)
-# #
-# # temp_list=data["temp"].to_list()
-# # print(len(temp_list))
-# # print(data["temp"].mean())
-# # print(data["temp"].max())
-# #
-# # # data columns
-# # print(data["condition"])
-# # print(data.condition)
-# # data in rows
-# # print(data[data.day=="Monday"])
-# # print(data[data.temp == data.temp.max()])
-# # moday= data[data.day=="Monday"]
-# # print(moday.condition)
-#
-#
-#  # create dataframe from scratch#
-# data_dict={
-#      "students":["amy","james","miftah"],
-#      "scores":[76,90,99]
-#  }
-# data1=pandas.DataFrame(data_dict)
-# print(data)
-# data.to_csv("new_data.csv")
-
-
+import turtle
 import pandas
-data= pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
-grey_squirrels_count=len(data[data["Primary Fur Color"]=="Gray"])
-red_squirrels_count=len(data[data["Primary Fur Color"]=="Cinnamon"])
-black_squirrels_count=len(data[data["Primary Fur Color"]=="Black"])
+screen = turtle.Screen()
 
-print(grey_squirrels_count)
-print(red_squirrels_count)
-print(black_squirrels_count)
-data_dict={
-    "Fur Color":["Gray","Cinnamon","Black"],
-    "Count":[grey_squirrels_count,red_squirrels_count,black_squirrels_count]
-}
-df=pandas.DataFrame(data_dict)
-df.to_csv("squirrel_count.csv")
+screen.title("U.S States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+data=pandas.read_csv("50_states.csv")
+all_states=data.state.to_list()
+guess_states=[]
+
+while len(guess_states)<50:
+
+    answer_state= screen.textinput(title="GUess the state", prompt="What's another state name").title()
+    print(answer_state)
+
+    if answer_state in all_states:
+        missing_states=[]
+        for state in all_states:
+            if state not in guess_states:
+                missing_states.append(state)
+        new_data=pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        guess_states.append(answer_state)
+        t=turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data= data[data.state==answer_state]
+        t.goto(int(state_data.x),int(state_data.y))
+        t.write(answer_state)
+
+
+
