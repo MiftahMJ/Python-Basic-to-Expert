7ea6357d55b8c8311c4f9aef8589dd5fd8d18609
Miftah Jabeen <avengerpak77@gmail.com>
2024-08-09 22:30:53 +0500

    Selenium

--- Day_48/main.py
+++ Day_48/main.py
@@ -1,7 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 
-# Corrected path to the ChromeDriver executable
+# Path to the ChromeDriver executable
 chrome_driver_path = r"C:\Users\Chaudhry Traders\Downloads\chromedriver-win64 (1)\chromedriver-win64\chromedriver.exe"
 
 # Initialize the Service object
@@ -10,10 +11,22 @@ service = Service(executable_path=chrome_driver_path)
 # Initialize the WebDriver with the Service object
 driver = webdriver.Chrome(service=service)
 
-# Open a webpage
-driver.get("https://www.amazon.com")
+# Navigate to the desired URL
+driver.get("https://www.python.org")
+print("Navigated to https://www.python.org")
 
-# Perform actions with the driver...
+# Correct way to find elements using CSS selector
+event_times = driver.find_elements(By.CSS_SELECTOR, ".event-widget time")
+event_names=driver.find_elements(By.CSS_SELECTOR, ".event-widget li a")
+events={}
+for n in range(len(event_times)):
+    events[n]={
+        "time": event_times[n].text,
+        "name":event_names[n].text,
+    }
+print(events)
+
+
 
 # Close the browser
 driver.quit()
